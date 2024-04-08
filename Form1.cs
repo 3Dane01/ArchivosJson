c30401fab9e9299cb5713de0a3f49608d131c021
Julia Dane <julia_chojolan@umes.edu.gt>
2024-04-08 09:14:26 -0600

    Agregar archivos de proyecto.

---
+++ Form1.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Archivos_Json
+{
+    public partial class Form1 : Form
+    {
+        List<int> notasTemporales = new List<int>();
+        List<NotasAlumno> listaNotas = new List<NotasAlumno>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int nota = Convert.ToInt32(textBoxNota.Text);
+            notasTemporales.Add(nota);
+        }
+        private void Grabar()
+        {
+            string json = JsonConvert.SerializeObject(listaNotas);
+            string archivo = "Datos.json";
+            System.IO.File.WriteAllText(archivo, json);
+        }
+        private void buttonGuadar_Click(object sender, EventArgs e)
+        {
+            //Guardar notas del alumno
+            NotasAlumno notasAlumno = new NotasAlumno();
+            notasAlumno.NombreAlumno = textBoxNombre.Text;
+            notasAlumno.Notas = notasTemporales.GetRange(0,notasTemporales.Count);
+            //guardar a ese almno en el listado de notas de alumnos
+            listaNotas.Add(notasAlumno);   
+            Grabar();
+            notasTemporales.Clear();
+        }
+    }
+}
---
+++ Notas.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Archivos_Json
+{
+    internal class NotasAlumno
+    {
+        string nombreAlumno;
+        List<int> notas;
+
+        public string NombreAlumno { get => nombreAlumno; set => nombreAlumno = value; }
+        public List<int> Notas { get => notas; set => notas = value; }
+    }
+}
